4eec0053be935ccabe12301e5134d86d8e18a56e
knt <kontadorlol@gmail.com>
2024-04-25 20:01:37 +0300

    android

--- src/components/hooks/useHealthData.tsx
+++ src/components/hooks/useHealthData.tsx
@@ -4,7 +4,13 @@ import AppleHealthKit, {
   HealthInputOptions,
   HealthUnit,
 } from "react-native-health";
+import {
+  initialize,
+  requestPermission,
+  readRecords,
+} from "react-native-health-connect";
 import { Platform } from "react-native";
+import { TimeRangeFilter } from "react-native-health-connect/lib/typescript/types/base.types";
 
 const permissions: HealthKitPermissions = {
   permissions: {
@@ -22,7 +28,7 @@ const useHealthData = (date: Date) => {
   const [steps, setSteps] = useState(0);
   const [flights, setFlights] = useState(0);
   const [distance, setDistance] = useState(0);
-
+  //----iOS
   useEffect(() => {
     if (Platform.OS !== "ios") {
       return;
@@ -91,6 +97,52 @@ const useHealthData = (date: Date) => {
     });
   }, [hasPermissions]);
 
+  //Android----------------------
+  const readSampleData = async () => {
+    // initialize the client
+    const isInitialized = await initialize();
+    if (!isInitialized) {
+      return;
+    }
+    // request permissions
+    await requestPermission([
+      { accessType: "read", recordType: "Steps" },
+      { accessType: "read", recordType: "FloorsClimbed" },
+      { accessType: "read", recordType: "Distance" },
+    ]);
+
+    const timeRangeFilter: TimeRangeFilter = {
+      operator: "between",
+      startTime: new Date(date.setHours(0, 0, 0, 0)).toISOString(),
+      endTime: new Date(date.setHours(23, 59, 59, 999)).toISOString(),
+    };
+    //steps
+    const steps = await readRecords("Steps", { timeRangeFilter });
+    const totalSteps = steps.reduce((sum, current) => sum + current.count, 0);
+    setSteps(totalSteps);
+    //distance
+    const distance = await readRecords("Distance", { timeRangeFilter });
+    const totalDistance = distance.reduce(
+      (sum, current) => sum + current.distance.inMeters,
+      0
+    );
+    setDistance(totalDistance);
+    //floors climbed
+    const floors = await readRecords("FloorsClimbed", { timeRangeFilter });
+    console.log(floors);
+    const totalFloors = floors.reduce(
+      (sum, current) => sum + current.floors,
+      0
+    );
+    setFlights(totalFloors);
+  };
+
+  useEffect(() => {
+    if (Platform.OS !== "android") {
+      return;
+    }
+    readSampleData();
+  }, []);
   return {
     steps,
     flights,
